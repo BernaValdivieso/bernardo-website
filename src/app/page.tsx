445558e44eb69caa449768fadd4712a9d071344f
Bernardo <bavaldivieso@uc.cl>
2024-06-19 14:11:05 -0400

    feat: add menu nav bar component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import { AboutSection } from "./components/about-section/about-section";
 import { HeroSection } from "./components/hero-section/hero-section";
-import { ProjectsSection } from "./components/projects-section/project-section";
+import { Navbar } from "./components/nav-bar/nav-bar";
+import { ProjectsSection } from "./components/projects-section/projects-section";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212]">
+      <Navbar />
       <div className="container mt-24 mx-auto px-12 py-4">
         <HeroSection />
         <section id="about">
